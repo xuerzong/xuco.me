799f325aaa75b79aecd3af3fe483df998c1c5001
xuerzong <xuerzong@gmail.com>
2025-05-05 16:54:25 +0800

    fix: reference path

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 import { type Metadata } from 'next'
 import { About } from './_components/about'
 import { Contact } from './_components/contact'
-import { Hero } from './_components/hero'
+import { Hero } from './_components/hero.new'
 
 export const generateMetadata = async () => {
   return {} as Metadata
